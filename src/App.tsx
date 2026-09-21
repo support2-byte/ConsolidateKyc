8434ea490b1ed37bf1cc4a9d7f30f40bda838826
saadsaifullahrgsl-rgb <saadsaifullah.rgsl@gmail.com>
2026-09-21 21:30:23 +0500

    refactor: simplify App component rendering by removing redundant ToastContainer and BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import KycRGSL from "./pages/KycForm";
 import KycMF from "./pages/KycFormMf";
 import KycCAS from "./pages/KycFormCas";
@@ -8,34 +8,38 @@ import InvoicePaymentForm from "./pages/InvoicePaymentForm";
 import DeliveryRequestForm from "./pages/DeliveryRequestPage";
 import StoragePurchaseForm from "./pages/StoragePurchaseForm";
 import DropOffRequestForm from "./pages/DropOffForm";
+import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="rgsl/:zohoId/:formId" element={<KycRGSL />} />
-      <Route path="messiah-freight/:zohoId/:formId" element={<KycMF />} />
-      <Route path="cas/:zohoId/:formId" element={<KycCAS />} />
-      <Route
-        path="booking-confirmation/:formId/:type"
-        element={<BookingConfirmationForm />}
-      />
-      <Route
-        path="/invoice-payment/:invoiceId"
-        element={<InvoicePaymentForm />}
-      />
-      <Route
-        path="/request-delivery/:itemRef"
-        element={<DeliveryRequestForm />}
-      />
-      <Route
-        path="/purchase-storage/:itemRef"
-        element={<StoragePurchaseForm />}
-      />
-      <Route
-        path="/drop-off-request/:itemRef"
-        element={<DropOffRequestForm />}
-      />
-      <Route path="unauthorized" element={<Unauthorized />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="rgsl/:zohoId/:formId" element={<KycRGSL />} />
+        <Route path="messiah-freight/:zohoId/:formId" element={<KycMF />} />
+        <Route path="cas/:zohoId/:formId" element={<KycCAS />} />
+        <Route
+          path="booking-confirmation/:formId/:type"
+          element={<BookingConfirmationForm />}
+        />
+        <Route
+          path="/invoice-payment/:invoiceId"
+          element={<InvoicePaymentForm />}
+        />
+        <Route
+          path="/request-delivery/:itemRef"
+          element={<DeliveryRequestForm />}
+        />
+        <Route
+          path="/purchase-storage/:itemRef"
+          element={<StoragePurchaseForm />}
+        />
+        <Route
+          path="/drop-off-request/:itemRef"
+          element={<DropOffRequestForm />}
+        />
+        <Route path="unauthorized" element={<Unauthorized />} />
+      </Routes>
+      <ToastContainer stacked position="bottom-right" theme="dark" />
+    </BrowserRouter>
   );
 }
